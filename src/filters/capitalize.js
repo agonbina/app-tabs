cb943dcc44f58a1de14d641ee3784d07784b1f09
Agon Bina <agon_bina@hotmail.com>
2015-06-30 13:54:11 +0200

    (syntax) capitalize filter syntax improvement

--- src/filters/capitalize.js
+++ src/filters/capitalize.js
@@ -1,8 +1,5 @@
 
 export default function(input, scope) {
-	if (input) {
-		return input.toLowerCase().substring(0,1).toUpperCase()+input.substring(1)
-	} else {
-		return input
-	}
+	if(input) input = input.toLowerCase().substring(0,1).toUpperCase()+input.substring(1)
+	return input
 }
