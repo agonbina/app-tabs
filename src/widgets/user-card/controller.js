1416a11a68e6a574e275f9f904dcdb7b3f75cbd0
Agon Bina <agon_bina@hotmail.com>
2015-07-02 10:53:09 +0200

    (chore) update the userCard directive to show the user interests

--- src/widgets/user-card/controller.js
+++
@@ -1,4 +0,0 @@
-
-export default $scope => {
-	console.log($scope)
-}
--- src/widgets/user-card/directive.js
+++ src/widgets/user-card/directive.js
@@ -1,14 +1,13 @@
 
-import Ctrl from './controller'
 import tpl from './template.jade'
 
 export default {
 	restrict: 'E',
 	replace: true,
-	controller: ['$scope', Ctrl],
 	scope: {
 		profile: '=',
-		preferences: '='
+		preferences: '=',
+		interests: '='
 	},
 	template: tpl,
 	link: ($scope, $el, attrs) => {
