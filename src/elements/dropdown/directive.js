26c35f961ab1783c3e2d0c752c0516f9cecb74bd
Agon Bina <agon_bina@hotmail.com>
2015-07-02 10:17:31 +0200

    (fix) setting the dropdown initial value was throwing

--- src/elements/dropdown/directive.js
+++ src/elements/dropdown/directive.js
@@ -17,19 +17,20 @@ export default {
 	},
 	link: function($scope, $el, attrs, ctrl) {
 		let $menu = $el[0].querySelector('.menu')
-		let findOption = (value, label) => $scope.options.find(option => option.value === value)
+		let findOption = value => $scope.options.find(option => option.value === value)
 		
+		ctrl.$render = () => {
+			let value = ctrl.$viewValue
+			if(value && value !== '') $scope.selected = findOption(value)
+		}
+
 		$scope.$watch('isOpen', isOpen => {
 			if(isOpen) $menu.setAttribute('style', 'display: block !important')
 			else $menu.removeAttribute('style')
 		})
-		$scope.$watch('selected', option => {
+		$scope.$watch('selected', (option={}) => {
 			$scope.isOpen = false
 			ctrl.$setViewValue(option.value)
 		})
-
-		ctrl.$render = () => {
-			if(ctrl.$viewValue !== '') $scope.selected = findOption(ctrl.$viewValue)
-		}
 	}
 }
